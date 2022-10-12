7716ca2160becd657fafa1528e3d1706eeaf97bf
nnzzll <nnzzll@zju.edu.cn>
2022-10-12 15:13:35 +0800

    ADD: Powell's method

---
+++ openMOTest/testPowell.cxx
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <openMO/openMO>
+#include <Eigen/Core>
+
+struct Functor
+{
+  int fcalls = 0;
+  double operator()(Eigen::VectorXd& x)
+  {
+    fcalls++;
+    return 100 * (x(1) - x(0) * x(0)) * (x(1) - x(0) * x(0)) + (1 - x(0)) * (1 - x(0));
+  }
+};
+
+int main(int argc, char** argv)
+{
+  Functor func;
+  Eigen::VectorXd x(2);
+  x << 0, 0;
+  mo::Powell<Functor> powell(2);
+  std::cout << powell.optimize(func, x) << std::endl;
+}
